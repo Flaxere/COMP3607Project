a63bd1eaa397cbe970709da78bc3bc10f09fa6c5
Flaxere <123189890+Flaxere@users.noreply.github.com>
2024-11-11 19:24:15 -0400

    Began initial work on making test more abstract

--- src/main/java/tempest_foundation/Testing/ConstructorTest.java
+++ src/main/java/tempest_foundation/Testing/ConstructorTest.java
@@ -13,11 +13,13 @@ public class ConstructorTest implements Test{
 
     private ClassDetails inTesting; 
     private ArrayList<Function> expectedConstructor;
+    ArrayList<String> testStrings;
 
-    public ConstructorTest(ArrayList<Function> expectedConstructor, ClassDetails inTesting) {
+    public ConstructorTest(ArrayList<Function> expectedConstructor, ClassDetails inTesting, ArrayList<String> testStrings) {
 
         this.inTesting = inTesting; 
         this.expectedConstructor = expectedConstructor;
+        this.testStrings=testStrings;
     }
 
     @Override
@@ -32,14 +34,37 @@ public class ConstructorTest implements Test{
             
             if (expectedFunc != 1){
                 ArrayList<String> content = currentFunction.getContent();
+                double grade = 1 / testStrings.size();
                 for (String data:content) {
-                    if (data.contains("ChatGPT-3.5") && data.contains("chatBotName") && currentFunction.hasVariable(new Variable("chatBotName","String"))) {
-                        inTesting.addGrade(1.0);
-                    } else {
-                        inTesting.addGrade(0);
-
-                        inTesting.addComment("The constructor was supposed to contain 'ChatGPT-3.5' as the default chatbotname.");
+                    String currentString = data.replaceAll("\\s", "");
+                    // "ChatGpt3.5-LLM"
+                    // notChatBotName=ChatGpt3.5-LLM
+                    int i=0;
+                    while(i < testStrings.size()){
+                        if(currentString.contains(testStrings.get(i))&& currentFunction.hasVariable(new Variable("chatBotName","String"))){
+                            inTesting.addGrade(grade);
+                            testStrings.remove(i);
+                            break;
+                        }else{
+                            inTesting.addGrade(0);
+                            inTesting.addComment("The constructor was supposed to contain " + " as the default chatbotname.");
+                        }
+            
                     }
+                    // for(String str:testStrings){
+                        
+                    //     if(currentString.contains(str.replaceAll("\\s", "" )) && data.contains("chatBotName") && currentFunction.hasVariable(new Variable("chatBotName","String"))){
+                            
+                    //     }
+                    // }
+
+                    // if (data.contains("ChatGPT-3.5") && data.contains("chatBotName") && currentFunction.hasVariable(new Variable("chatBotName","String"))) {
+                    //     inTesting.addGrade(1.0);
+                    // } else {
+                    //     inTesting.addGrade(0);
+
+                    //     inTesting.addComment("The constructor was supposed to contain 'ChatGPT-3.5' as the default chatbotname.");
+                    // }
                 }
             }
         } 
