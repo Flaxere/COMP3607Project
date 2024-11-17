8c523a2a5c463f1d5237b64be5c7819c77046198
Flaxere <123189890+Flaxere@users.noreply.github.com>
2024-11-17 13:47:20 -0400

    added Minor formatting change

--- src/main/java/tempest_foundation/App.java
+++ src/main/java/tempest_foundation/App.java
@@ -1,21 +1,9 @@
 package tempest_foundation;
 
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-
 import tempest_foundation.ClassElements.AutoGrader;
-import tempest_foundation.ClassElements.ClassDetails;
-import tempest_foundation.ClassElements.Function;
-import tempest_foundation.ClassElements.Variable;
-import tempest_foundation.ClassElements.Visibility;
 import tempest_foundation.SubmissionElements.Submission;
-import tempest_foundation.Testing.AccessorTest;
-import tempest_foundation.Testing.CompliationCheck;
-import tempest_foundation.Testing.VariableTest;
+
 /**
  * Hello Jura-Tempest Federation!
  *
@@ -31,9 +19,6 @@ public class App
         AutoGrader grader = new AutoGrader();
 
         grader.processAssignments(submissions);
-
-        
-        // System.out.println(s.getClass(0).getTotalGrade());
 
     }
 }
